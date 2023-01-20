2c602057ea8a842742359220bfee352f50e77ab5
filipesenaifirjan <filipemattossenai@gmail.com>
2023-01-20 10:45:33 -0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: { loader: "babel-loader"}
+            },
+            {
+                test: /\.html$/,
+                use: [{ loader: "html-loader"}]
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"]
+                    },
+                    {
+                        test: /\.(png|jpe?g\gif)$/i,
+                        use: ["file-loader"]
+                        },
+        ]
+
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    plugin: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
